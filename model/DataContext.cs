9d5a0be00909c77a201fb2a7e9e664a9adbeb347
cris-hub <cecardona14@misena.edu.co>
2018-05-10 08:23:11 -0500

    error solucionado

--- model/DataContext.cs
+++ model/DataContext.cs
@@ -41,4 +41,19 @@ namespace model
 
     }
 
+    public class TemporaryDbContextFactory : IDesignTimeDbContextFactory<DataContext>
+    {
+        //////// 
+        public DataContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<DataContext>();
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+
+              .Build();
+
+            builder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=curso;Trusted_Connection=True;");
+            return new DataContext(builder.Options);
+        }
+    }
+
 }
